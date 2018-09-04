43830a9bf80131b750a53ec9d88f539bdc8a9171
Tettel <RUTettel@gmail.com>
2018-09-04 14:31:28 +0500

    гыг

--- bot.py
+++ bot.py
@@ -6,26 +6,25 @@ import re
 
 app = Client(
     session_name="my_account",
-    api_id=YOUR ID,
-    api_hash="YOUR HASH"
+    api_id=<YOUR ID>,
+    api_hash="<YOUR HASH>"
 )
 
 app = Client("my_account")
-
 app.start() #запускаем клиент
 
-@app.on_message(Filters.regex("кинь (монетку|монету)", re.I)) #регулярное выражение(можно изменить на свою фразу)
+@app.on_message(Filters.regex("кинь (монетку|монету)", re.I) ~Filters.chat(<туть ваш ID>))
 def my_handler(client, message):
     ch_id = message.chat.id #Переменные для облегчения работы
     us_id = message.from_user.id
     ms_id = message.message_id
+    
     ran_num = random.randint(1, 2) #Сам рандомайзер(можно вставить свои значения)
-    if us_id == 377703807: #ВСТАВИТЬ СВОЙ ID
-        pass #Создано для того, чтобы бот не отвечал на свои сообщения
-    else:
-        if ran_num == 1: #Если вышло число 1, выпадает решка.
-            app.send_message(chat_id=ch_id, text=f'<b>Выпала:</b> <b>Решка</b>', reply_to_message_id=ms_id, parse_mode='HTML')
-        else: #Если другое число - орел
-            app.send_message(chat_id=ch_id, text=f'<b>Выпал:</b> <b>Орел</b>', reply_to_message_id=ms_id, parse_mode='HTML')
+     if ran_num == 1: #Если вышло число 1, выпадает решка.
+         res = '<b>Выпал: орел</b>'
+     else: #Если другое число - орел
+         res = '<b>Выпала: решка</b>'
+     app.send_message(chat_id=ch_id, text=f'<b>Выпал:</b>' + res, 
+                      reply_to_message_id=ms_id, parse_mode='HTML')
 
 app.idle()
